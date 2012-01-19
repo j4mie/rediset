2d1ae568076aebaa78975225eca57d02f6845ca2
Jamie Matthews <jamie.matthews@gmail.com>
2012-01-19 11:15:07 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='rediset',
+    version='0.0.1',
+    description='Redis set abstraction',
+    author='Jamie Matthews',
+    author_email='jamie.matthews@gmail.com',
+    url='http://github.com/j4mie/rediset',
+    py_modules=['rediset'],
+)
