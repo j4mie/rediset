e6d4cb92cbfa9ee733d357d8380dfca8dfdee0d9
Jamie Matthews <jamie.matthews@gmail.com>
2012-01-19 09:42:42 +0000

    Initial commit

---
+++ rediset.py
@@ -0,0 +1,117 @@
+import redis
+
+
+class Rediset(object):
+
+    def __init__(self, key_prefix=None):
+        self.connection = RedisConnection(key_prefix)
+
+    def set(self, key):
+        return Set(self.connection, key)
+
+    def intersection(self, *items):
+        return Intersection(self.connection, items)
+
+    def union(self, *items):
+        return Union(self.connection, items)
+
+
+class RedisConnection(object):
+
+    def __init__(self, key_prefix=None):
+        self.redis = redis.Redis()
+        self.key_prefix = key_prefix
+
+    def create_key(self, original_key):
+        key = original_key
+        if self.key_prefix:
+            key = "%s:%s" % (self.key_prefix, key)
+        return key
+
+    def scard(self, key):
+        key = self.create_key(key)
+        return self.redis.scard(key)
+
+    def sadd(self, key, *values):
+        key = self.create_key(key)
+        return self.redis.sadd(key, *values)
+
+    def smembers(self, key):
+        key = self.create_key(key)
+        return self.redis.smembers(key)
+
+    def sinterstore(self, dest, keys):
+        dest = self.create_key(dest)
+        keys = [self.create_key(key) for key in keys]
+        return self.redis.sinterstore(dest, keys)
+
+    def sunionstore(self, dest, keys):
+        dest = self.create_key(dest)
+        keys = [self.create_key(key) for key in keys]
+        return self.redis.sunionstore(dest, keys)
+
+
+class Node(object):
+
+    def cardinality(self):
+        return self.connection.scard(self.key)
+
+    def __len__(self):
+        self.create()
+        return self.cardinality()
+
+    def __repr__(self):
+        return "<%s.%s %s>" % (__name__, self.__class__.__name__, self.key)
+
+    def members(self):
+        self.create()
+        return self.connection.smembers(self.key)
+
+    def create_children(self):
+        [child.create() for child in self.children]
+
+    def child_keys(self):
+        return sorted(child.key for child in self.children)
+
+
+class Set(Node):
+
+    def __init__(self, connection, key):
+        self.connection = connection
+        self.key = key
+
+    def add(self, *values):
+        self.connection.sadd(self.key, *values)
+
+    def create(self):
+        pass
+
+
+class Intersection(Node):
+
+    def __init__(self, connection, children):
+        self.connection = connection
+        self.children = children
+
+    @property
+    def key(self):
+        return "intersection(%s)" % ",".join(self.child_keys())
+
+    def create(self):
+        self.create_children()
+        return self.connection.sinterstore(self.key, self.child_keys())
+
+
+class Union(Node):
+
+    def __init__(self, connection, children):
+        self.connection = connection
+        self.children = children
+
+    @property
+    def key(self):
+        return "union(%s)" % ",".join(self.child_keys())
+
+    def create(self):
+        self.create_children()
+        return self.connection.sunionstore(self.key, self.child_keys())
---
+++ tests.py
@@ -0,0 +1,121 @@
+from unittest import TestCase
+from rediset import Rediset, RedisConnection
+
+
+class KeyGenerationTestCase(TestCase):
+
+    def test_key_generation(self):
+        rc = RedisConnection(key_prefix='some-prefix')
+        key = rc.create_key('foo')
+        self.assertEqual(key, 'some-prefix:foo')
+
+
+class RedisTestCase(TestCase):
+
+    PREFIX = 'rediset-tests'
+
+    def setUp(self):
+        self.rediset = Rediset(key_prefix=self.PREFIX)
+
+    def tearDown(self):
+        redis = self.rediset.connection.redis
+        keys = redis.keys('%s*' % self.PREFIX)
+        redis.delete(*keys)
+
+
+class SetTestCase(RedisTestCase):
+
+    def test_basic_set(self):
+        s = self.rediset.set('key')
+
+        s.add('a')
+        s.add('b')
+        s.add('c')
+
+        self.assertEqual(len(s), 3)
+        self.assertEqual(s.members(), set(['a', 'b', 'c']))
+
+
+class IntersectionTestCase(RedisTestCase):
+
+    def test_basic_intersection(self):
+        s1 = self.rediset.set('key1')
+        s2 = self.rediset.set('key2')
+
+        s1.add('a', 'b')
+        s2.add('b', 'c')
+
+        i = self.rediset.intersection(s1, s2)
+        self.assertEqual(len(i),1)
+        self.assertEqual(i.members(), set(['b']))
+
+    def test_intersection_tree(self):
+        s1 = self.rediset.set('key1')
+        s2 = self.rediset.set('key2')
+        s3 = self.rediset.set('key3')
+
+        s1.add('a', 'b', 'c')
+        s2.add('b', 'c', 'd')
+        s3.add('b', 'z', 'x')
+
+        i1 = self.rediset.intersection(s1, s2)
+        self.assertEqual(len(i1), 2)
+
+        i2 = self.rediset.intersection(i1, s3)
+        self.assertEqual(len(i2), 1)
+        self.assertEqual(i2.members(), set(['b']))
+
+
+class UnionTestCase(RedisTestCase):
+
+    def test_basic_union(self):
+        s1 = self.rediset.set('key1')
+        s2 = self.rediset.set('key2')
+
+        s1.add('a', 'b')
+        s2.add('b', 'c')
+
+        u = self.rediset.union(s1, s2)
+        self.assertEqual(len(u), 3)
+        self.assertEqual(u.members(), set(['a', 'b', 'c']))
+
+    def test_union_tree(self):
+        s1 = self.rediset.set('key1')
+        s2 = self.rediset.set('key2')
+        s3 = self.rediset.set('key3')
+
+        s1.add('a', 'b', 'c')
+        s2.add('b', 'c', 'd')
+        s3.add('b', 'z', 'x')
+
+        i1 = self.rediset.union(s1, s2)
+        self.assertEqual(len(i1), 4)
+
+        i2 = self.rediset.union(i1, s3)
+        self.assertEqual(len(i2), 6)
+        self.assertEqual(i2.members(), set(['a', 'b', 'c', 'd', 'z', 'x']))
+
+
+class CombinationTestCase(RedisTestCase):
+
+    def test_complex_tree(self):
+        s1 = self.rediset.set('key1')
+        s2 = self.rediset.set('key2')
+        s3 = self.rediset.set('key3')
+        s4 = self.rediset.set('key4')
+        s5 = self.rediset.set('key5')
+
+        s1.add('a', 'b')
+        s2.add('b', 'c')
+        s3.add('b', 'd')
+        s4.add('e', 'f')
+        s5.add('b', 'z')
+
+        result = self.rediset.union(
+            self.rediset.intersection(s1, s2, s3),
+            s4,
+            s5
+        )
+
+        self.assertEqual(len(result), 4)
+        self.assertEqual(result.members(), set(['b', 'e', 'f', 'z']))
