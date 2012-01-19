60b0af564eda1e8a6fe719fe27e034a86c38be8b
Jamie Matthews <jamie.matthews@gmail.com>
2012-01-19 16:46:33 +0000

    Edit description

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 setup(
     name='rediset',
     version='0.0.1',
-    description='Redis lazy set abstraction',
+    description='Composable, cacheable, lazy trees of Redis set operations',
     author='Jamie Matthews',
     author_email='jamie.matthews@gmail.com',
     url='http://github.com/j4mie/rediset',
